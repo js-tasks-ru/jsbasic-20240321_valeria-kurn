32594b38ca6fca8c03dd66801da3e07a4e5a43a8
valeriakurn <valeriyak@yandex.ru>
2024-04-03 19:26:51 +0300

    3rd module - tasks update

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,10 +1,3 @@
 function camelize(str) {
-  let words = str.split('-').map((word, index) => {
-    if (index != 0) {
-      word =  word[0].toUpperCase() + word.slice(1)
-    }
-    return word
-  })
-
-  return words.join('')
+  return words = str.split('-').map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('')
 }
