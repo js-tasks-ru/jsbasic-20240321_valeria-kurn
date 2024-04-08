77148334cc413f445a648f07bbf9030665502f95
valeriakurn <valeriyak@yandex.ru>
2024-04-08 11:51:34 +0300

    4th module - 1st task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let friendsList = friends.map(friend => friend.firstName + ' ' + friend.lastName)
+  let ul = document.createElement('ul');
+  
+  for (let friend of friendsList) {
+    ul.innerHTML = ul.innerHTML + `<li>${friend}</li>`
+  }
+
+  return ul
 }
